dfe4b4b39886cbb1f470f7071655710cdfd06226
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-07 20:57:05 -0500

    Added javascript functionality
    Changed background, added reset button. Added the javascript to allow
    for the Xs and Os to be placed and notification is there is already
    something in that space.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
+var playerTurn = "X";
 
+function clickMe(elementId)	{
+	document.getElementById("errors").style.display = "none";
+	if(elementId.innerHTML == "X" || elementId.innerHTML == "O")	{
+		document.getElementById("errors").style.display = "block";
+		return;
+	}
+	else	{
+		elementId.innerHTML = playerTurn;
+	}
 
+	if (playerTurn === "X") {
+		playerTurn = "O";
+		document.getElementById("status-turn").innerHTML = playerTurn;
+	}
+	else	{
+		playerTurn = "X";
+		document.getElementById("status-turn").innerHTML = playerTurn;
+	}
+}
 
 
 
